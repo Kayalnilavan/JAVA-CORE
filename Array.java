5ea4a5679a1a0de47210b801a241ac5b4853668c
Kayalnilavan <kayalnilavan110@gmail.com>
2026-01-21 14:26:19 +0530

    Add initial file

---
+++ Array.java
@@ -0,0 +1,16 @@
+class Array{
+	public static void main(String args[]){
+
+		int[]x;
+		x= new int[6];
+		x[0]=12;
+		x[1]=55;
+		x[2]=36;
+		x[3]=54;
+		x[4]=87;
+		
+		System.out.println(x[2]);
+		System.out.println(x[4]);
+		System.out.println(x[0]);
+	}
+}
