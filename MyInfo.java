815da4f3b7e4de8253ee29c59df0c75035e87af8
Kayalnilavan <kayalnilavan110@gmail.com>
2026-01-22 14:35:36 +0530

    Add initial file

---
+++ MyInfo.java
@@ -0,0 +1,16 @@
+class MyInfo{
+	public static void main (String args []){
+	
+	String fname;
+	String lname;
+	
+	fname= "AMD";
+	lname= "RYZEN";
+	
+	System.out.println("*******************");
+	System.out.println("My Info");
+	System.out.println("First Name :" + fname);
+	System.out.println("Last Name :" + lname);
+	System.out.println("*******************");
+ }
+}
