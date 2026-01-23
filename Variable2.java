68255fd4e832c31c9baba7b4777f32c0ac6942cf
Kayalnilavan <kayalnilavan110@gmail.com>
2026-01-23 23:50:51 +0530

    Add variable2 file

---
+++ Variable2.java
@@ -0,0 +1,68 @@
+class Variable2{
+	public static void main(String args[]){
+		
+		byte by1='A';
+		System.out.println("Byte : "+by1);
+		
+		byte by2=65;
+		System.out.println("Byte : "+by2);
+		
+		byte by3=0101;
+		System.out.println("Byte : "+by3);
+		
+		byte by4=0x41;
+		System.out.println("Byte : "+by4);
+		
+		byte by5='\u0041';
+		System.out.println("Byte : "+by5);
+		
+		byte by6=0b1000001;
+		System.out.println("Byte : "+by6);
+		
+
+
+		char ch='A';
+		System.out.println("Char : "+ch);
+		
+		char ch1=65;
+		System.out.println("Char : "+ch1);
+		
+		char ch2=0101;
+		System.out.println("Char : "+ch2);
+		
+		char ch3=0x41;
+		System.out.println("Char : "+ch3);
+		
+		char ch4='\u0041';
+		System.out.println("Char : "+ch4);
+		
+		char ch5=0b1000001;
+		System.out.println("Char : "+ch5);
+
+
+		float fl;
+		fl=10.5f;
+		System.out.println("Float : "+fl);
+
+		double db;
+		db=67.9D;
+		System.out.println("Double : "+db);
+
+		byte by;
+		by=127;
+		System.out.println("Byte : "+by);
+		
+		short sh;
+		sh=50;
+		System.out.println("Short : "+sh);
+		
+		int in;
+		in=2324;
+		System.out.println("Int : "+in);
+		
+		long lg;
+		lg=2324124346747774l;
+		System.out.println("Long : "+lg);
+
+	}
+}
