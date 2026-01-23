ae275886b3e2e1b9fc6d567077c3a9566b93acf4
Kayalnilavan <kayalnilavan110@gmail.com>
2026-01-23 23:19:16 +0530

    Add variables file

---
+++ Variables.java
@@ -0,0 +1,45 @@
+class Variables{
+	public static void main (String args []){
+		
+		float fl;
+		fl=10.5F;
+		System.out.println("Float is: "+fl);
+		
+		float fl2;
+		fl2=89.3f;
+		System.out.println("Float is : "+fl2);
+
+		double db;
+		db=89.5D;
+		System.out.println("Double is : "+db);
+		
+		double db2;
+		db2=74.5d;
+		System.out.println("Double is : "+db2);
+		
+		double db3;
+		db3=59.2;
+		System.out.println("Double : "+db3);
+
+		byte by;
+		by=119;
+		System.out.println("byte is "+by);
+		
+		short sh;
+		sh=30;
+		System.out.println("Short is "+sh);
+		
+		char ch1='B';
+		System.out.println("char :" +ch1);
+		
+		long lg;
+		lg=41557457l;
+		System.out.println("long is :" +lg);
+		
+		boolean bool=false;
+		System.out.println(bool);
+		
+		boolean bool2=true;
+		System.out.println(bool2);
+	}
+}
