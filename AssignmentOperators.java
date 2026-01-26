4a8a65055e2311cb11fbe9a58f1736939bf3c3f3
Kayalnilavan <kayalnilavan110@gmail.com>
2026-01-26 23:53:09 +0530

    Add  Assignment Operators file

---
+++ AssignmentOperators.java
@@ -0,0 +1,21 @@
+class AssignmentOperators{
+	public static void main(String args[]){
+		int x = 10;
+		System.out.println("x = " + x);
+		
+		x += 5;
+		System.out.println("x += 5 : " + x);
+		
+		x -= 5;
+		System.out.println("x -= 5 : " + x);
+		
+		x *= 5;
+		System.out.println("x *= 5 : " + x);
+		
+		x /= 5;
+		System.out.println("x /= 5 : " + x);
+		
+		x %= 5;
+		System.out.println("x %= 5 : " + x);
+	}
+}
