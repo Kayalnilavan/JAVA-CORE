bd367bad9ce6e5590da4a82e3a0a270c6c6d4448
Kayalnilavan <kayalnilavan110@gmail.com>
2026-02-02 10:48:28 +0530

    Add Array For Loop file

---
+++ Arrayforloop.java
@@ -0,0 +1,9 @@
+class Arrayforloop{
+	public static void main(String args[]){
+		
+		int[] marks={14,52,56,74,84,65};
+		for(int x=0;x<marks.length;x++){
+			System.out.println(marks[x]);
+		}
+	}
+}
---
+++ Arrayforloop2.java
@@ -0,0 +1,9 @@
+class Arrayforloop2{
+	public static void main(String args[]){
+		
+		int[] marks={14,52,56,74,84,65};
+		for(int y=marks.length-1;y>0;y--){
+			System.out.println(marks[y]);
+		}
+	}
+}
