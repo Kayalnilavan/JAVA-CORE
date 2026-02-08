fbd6ffd71699babff49b30bdd4949c313e9b0eb0
Kayalnilavan <kayalnilavan110@gmail.com>
2026-02-08 23:09:56 +0530

    Add array While Loop file

---
+++ Arraywhileloop.java
@@ -0,0 +1,11 @@
+class Arraywhileloop{
+	public static void main(String args[]){
+		
+		String[] subjects={"Maths","Science","English","Tamil"};
+		int i=1;
+    	while(i<=subjects.length){
+			System.out.println(subjects[i]);
+			i++;
+		}
+	}
+}
