bf9ad1daa6bb1c5454484833a4a329c084ff889e
Kayalnilavan <kayalnilavan110@gmail.com>
2026-02-12 23:11:56 +0530

    Add TwoDarray2 demo for 2D array access
    Add TwoDarray2.java example that defines a 2D int array and prints two elements (x[0][3] and x[1][0]). This file demonstrates basic creation and indexing of a two-dimensional array.

---
+++ Two D Array/TwoDarray2.java
@@ -0,0 +1,10 @@
+class TwoDarray2{
+	public static void main(String args[]){
+		
+		int[][] x={{1,5,7,2,5},{6,4,1,0,9}};
+		
+		System.out.println(x[0][3]);
+		System.out.println(x[1][0]);
+		
+	}
+}
