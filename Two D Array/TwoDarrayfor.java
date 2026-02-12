4fedfeefdb971ed704cdb1ad840dc4020b28fc93
Kayalnilavan <kayalnilavan110@gmail.com>
2026-02-12 23:12:24 +0530

    Add TwoDarrayfor.java demo printing 2D array
    Add a new Java class TwoDarrayfor that demonstrates iterating over and printing elements of a 2x5 int 2D array. The program defines int[][] x = {{1,5,7,2,5},{6,4,1,0,9}} and uses nested for loops (i<=1, j<=4) to print each element on its own line.

---
+++ Two D Array/TwoDarrayfor.java
@@ -0,0 +1,11 @@
+class TwoDarrayfor{
+	public static void main(String args[]){
+		int[][] x={{1,5,7,2,5},{6,4,1,0,9}};
+		
+		for(int i=0;i<=1;i++){
+			for(int j=0;j<=4;j++){
+				System.out.println(x[i][j]);
+			}
+		}
+	}
+}
