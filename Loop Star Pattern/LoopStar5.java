bd60e9b0e859520fe75ce95bb711bdd567e25320
Kayalnilavan <kayalnilavan110@gmail.com>
2026-02-13 01:11:17 +0530

    Add Loop Star 5.java star pattern
    Introduce LoopStar5.java, a Java program that prints a centered inverted pyramid of stars (height 5) using nested for-loops to handle leading spaces and symmetric star printing. Adds a simple main method demonstrating loop control for pattern output.

---
+++ Loop Star Pattern/LoopStar5.java
@@ -0,0 +1,17 @@
+class LoopStar5{
+	public static void main(String args[]){
+		int i,j;
+		for(i=5;i>0;i--){
+			for(j=5;j>i;j--){
+				System.out.print(" ");
+			}
+			for(j=1;j<=i;j++){
+				System.out.print("*");
+			}
+			for(j=1;j<i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
